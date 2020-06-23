33d1f3a38f834e5a85601bdee69b2901cdbaec58
Tarawanti <tarawanti1@gmail.com>
2020-06-23 09:25:43 -0400

    XUnit

---
+++ LearningCSharpSolution/LearningCSharp/TestingBasics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningCSharp
+{
+    class TestingBasics
+    {
+        public void CanAddTwoNumbers()
+        { }
+    }
+}
