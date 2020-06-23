6ebe041025bcef2a0d762421e8aa4728d8a5a724
Tarawanti <tarawanti1@gmail.com>
2020-06-23 09:11:25 -0400

    first commit

---
+++ ConsoleAppSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+ 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            //adding new line 
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            //Console.WriteLine("You name is :"+name);
+            Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+            int intAge = Convert.ToInt32(age);
+            //Console.WriteLine("Your age is :" + age);
+            Console.WriteLine("Enter your Favorite Colo");
+            string FavtColor = Console.ReadLine();
+           // Console.BackgroundColor = ConsoleColor.Blue;
+           // Console.Clear();
+
+
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), FavtColor);
+            Console.Clear();
+
+
+
+            if (intAge>=21)
+            {
+                Console.WriteLine(" have Beer if You want");
+                Console.WriteLine("\n\n You name is :" + name);
+                Console.WriteLine("Your age is :" + age);
+                Console.WriteLine("Your Favorite Color  is :" + FavtColor);
+               
+
+            }
+            else 
+            {
+
+                Console.WriteLine(" have Root Beer");
+
+                Console.WriteLine("\n\n You name is :" + name);
+                Console.WriteLine("Your age is :" + age);
+                Console.WriteLine("Your Favorite Color  is :" + FavtColor);
+            }
+
+
+
+
+        }
+    }
+}
+
+
